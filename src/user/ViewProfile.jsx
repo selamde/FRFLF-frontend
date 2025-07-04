e10a590b9e1857609fb3ea11f086fc6532e54060
Selamsew Dagne <168326591+selamde@users.noreply.github.com>
2025-07-04 16:39:40 +0300

    Update ViewProfile.jsx

--- src/user/ViewProfile.jsx
+++ src/user/ViewProfile.jsx
@@ -29,7 +29,7 @@ const ViewProfile = () => {
                 <div className='w-full'>
                     <div className='flex md:flex-row  flex-col items-center  gap-10 border-b border-blue-500 p-2 mb-4'>
                         <div>
-                            <img className='w-[200px] h-[200px] rounded-full' src={`http://localhost:3001/${user.image}`} alt="" />
+                            <img className='w-[200px] h-[200px] rounded-full' src={`https://frflf-backend.onrender.com/${user.image}`} alt="" />
                         </div>
                         <div>
                             <h1 className='md:text-4xl text-2xl font-bold'>{user.name}</h1>
